88ecc4210fbab102a4420e622a7aa583b93e4d2d
gustavo <gustavo@aswti.com.br>
2025-11-14 16:11:30 -0300

    Contador evoluido

--- react-fundamentos/pages/estado/com.tsx
+++ react-fundamentos/pages/estado/com.tsx
@@ -5,13 +5,22 @@ import { IconMinus, IconPlus } from "@tabler/icons-react";
 
 export default function PaginaComEstado() {
   const [numero, setNumero] = useState(0)
+  const [delta, setDelta] = useState(0)
 
-  function incrementar() {
-    setNumero(numero + 1)
+  function decrementarDelta() {
+    setDelta(delta - 1)
   }
 
+  function incrementarDelta() {
+    setDelta(delta + 1)
+  }
+  
   function decrementar() {
-    setNumero(numero - 1)
+    setNumero(numero - delta)
+  }
+
+  function incrementar() {
+    setNumero(numero + delta)
   }
 
   return (
@@ -24,6 +33,15 @@ export default function PaginaComEstado() {
           </button>
           <button onClick={incrementar} className="bg-purple-800 p-4 rounded-full">
             <IconPlus size={80} stroke={1} />
+          </button>
+        </div>
+        <div className="flex gap-4 justify-center items-center">
+          <button onClick={decrementarDelta} className="bg-blue-400 rounded-full p-4">
+            <IconMinus size={40} stroke={1} />
+          </button>
+          <span>{delta}</span>
+          <button onClick={incrementarDelta} className="bg-blue-800 rounded-full p-4">
+            <IconPlus size={40} stroke={1} />
           </button>
         </div>
       </div>
