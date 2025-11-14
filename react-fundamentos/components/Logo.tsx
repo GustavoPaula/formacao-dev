dc36e0f56289be54e736cf846dda99689ffd7413
gustavo <gustavo@aswti.com.br>
2025-11-14 10:47:44 -0300

    página inicial

--- react-fundamentos/components/Logo.tsx
+++ react-fundamentos/components/Logo.tsx
@@ -1,10 +1,11 @@
 import { IconBrandReact } from "@tabler/icons-react";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <div className="flex px-5 items-center gap-2.5 font-black h-16 bg-purple-700">
+    <Link href="/" className="flex px-5 items-center gap-2.5 font-black h-16 bg-purple-700">
       <span><IconBrandReact size={30} stroke={1} /></span>
       <span className="text-2xl">React</span>
-    </div>
+    </Link>
   )
 }
